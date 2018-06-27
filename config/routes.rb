3094fd19fdf3e6d86a13eec8946925b16a285d31
tomo <arm_93_0618@yahoo.co.jp>
2018-06-27 12:12:46 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'messages/:id', to: 'messages#show'
+  #post 'messages', to: 'messages#create'
+  #put 'messages/:id', to: 'messages#update'
+  #delete 'messages/:id', to: 'messages#destroy'
+  
+  #index: showの補助ページ
+  #get 'messages', to: 'messages#index'
+  #new 新規作成用のフォームページ
+  #get 'messages/new', to: 'messages#new'
+  # edit: 更新用のフォームページ
+  #get 'messages/:id/edit', to: 'messages#edit'
+  
+  #全て以下で代用できる
+  root to: 'messages#index'
+  
+  resources :messages
+
 end
+
